8d0d5b4a9efd4554a1d929b7f95d5f446bb08c2a
Heather <81587916+heathermrauch@users.noreply.github.com>
2022-06-23 15:10:37 -0400

    Update parse_encoded_string.py

--- parse_encoded_string.py
+++ parse_encoded_string.py
@@ -1,9 +1,23 @@
+'''
+Parse an encoded string
 
-# Parse an encoded string
-# Accepts an encoded string containing first name, last name and id separated
-# by an undetermined count of zeros
-# Returns a dictionary of parsed values
-# Original Challenge found here: https://www.codecademy.com/resources/blog/advanced-python-code-challenges/
+In this Python challenge, you need to write a function that accepts an encoded
+string as a parameter. This string will contain a first name, last name, and an
+id.
+
+Values in the string can be separated by any number of zeros. The id is a
+numeric value but will contain no zeros. The function should parse the string
+and return a Python dictionary that contains the first name, last name, and id
+values.
+
+An example input would be "Robert000Smith000123". The function should return
+the following using that input:
+
+{ "first_name": "Robert", "last_name": "Smith", "id": "123" }
+
+Original Challenge found here: 
+https://www.codecademy.com/resources/blog/advanced-python-code-challenges/
+'''
 
 
 def parse_str(string:str):
