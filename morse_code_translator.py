6a8612a6a9674c3b2b448e5524b49709d2a7223a
Heather <81587916+heathermrauch@users.noreply.github.com>
2022-06-23 13:22:00 -0400

    Update morse_code_translator.py

--- morse_code_translator.py
+++ morse_code_translator.py
@@ -1,23 +1,51 @@
+
 # Morse code translator
 # accepts translatable characters as input
 # returns Morse code equivalent
+# Original Challenge found here: https://www.codecademy.com/resources/blog/advanced-python-code-challenges/
 
-MORSE_CODE = {
-    'A': '.-',     'B': '-...',   'C': '-.-.',   'D': '-..',
-    'E': '.',      'F': '..-.',   'G': '--.',    'H': '....',
-    'I': '..',     'J': '.---',   'K': '-.-',    'L': '.-..',
-    'M': '--',     'N': '-.',     'O': '---',    'P': '.--.',
-    'Q': '--.-',   'R': '.-.',    'S': '...',    'T': '-',
-    'U': '..-',    'V': '...-',   'W': '.--',    'X': '-..-',
-    'Y': '-.--',   'Z': '--..',   '1': '.----',  '2': '..---',
-    '3': '...--',  '4': '....-',  '5': '.....',  '6': '-....',
-    '7': '--...',  '8': '---..',  '9': '----.',  '0': '-----',
-    ',': '--..--', '?': '..--..', ':': '---...', '&': '.-...',
-    ' ': ' '
+MORSE_CODE = { 
+    'A':'.-',      'B':'-...',    'C':'-.-.',     'D':'-..',
+    'E':'.',       'F':'..-.',    'G':'--.',      'H':'....',
+    'I':'..',      'J':'.---',    'K':'-.-',      'L':'.-..',
+    'M':'--',      'N':'-.',      'O':'---',      'P':'.--.',
+    'Q':'--.-',    'R':'.-.',     'S':'...',      'T':'-',
+    'U':'..-',     'V':'...-',    'W':'.--',      'X':'-..-',
+    'Y':'-.--',    'Z':'--..',    '1':'.----',    '2':'..---',
+    '3':'...--',   '4':'....-',   '5':'.....',    '6':'-....',
+    '7':'--...',   '8':'---..',   '9':'----.',    '0':'-----',
+    ',':'--..--',  ':': '---...', '\'': '.----.', '.': '.-.-.-',
+    '!': '-.-.--', '?': '..--..'
 }
 
-def morse_code(english:str):
-    return ''.join([MORSE_CODE.get(x.upper(), '') for x in english])
 
-if __name__ == '__main__'"
-    print(morse_code('Hello! My name is Heather!'))
+def to_morse_code(letters:str):
+    '''Converts a string to Morse Code
+    Required parameters:
+    letters (str): the string to translate to Morse Code
+    '''
+    encoded = ' '.join([MORSE_CODE.get(x.upper(), '') for x in letters])
+    return encoded
+
+def from_morse_code(morse_code:str):
+    '''Converts Morse Code to a string
+    Required parameters:
+    morse_code (str): the Morse Code to translate to letters
+    '''
+    parsed = [x.split() for x in morse_code.split('  ')]
+    decoded = ''
+    for word in parsed:
+        for letter in word:
+            decoded += [k for k, v in MORSE_CODE.items() if v == letter][0]
+        decoded += ' '
+    return decoded
+
+
+if __name__ == '__main__':
+    
+    message = 'This is an english string'
+    encoded = to_morse_code(message)
+    decoded = from_morse_code(encoded)
+
+    print('Encoded Message:', encoded)
+    print('Decoded Message:', decoded)
