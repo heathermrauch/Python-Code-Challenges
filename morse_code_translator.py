d40ee1c750d508e9f89fc4d7a1f7faaebbc10b51
Heather <81587916+heathermrauch@users.noreply.github.com>
2022-06-23 12:17:51 -0400

    Create morse_code_translator.py

---
+++ morse_code_translator.py
@@ -0,0 +1,22 @@
+# Morse code translator
+# accepts translatable characters as input
+# returns Morse code equivalent
+
+MORSE_CODE = {
+    'A': '.-',     'B': '-...',   'C': '-.-.',   'D': '-..',
+    'E': '.',      'F': '..-.',   'G': '--.',    'H': '....',
+    'I': '..',     'J': '.---',   'K': '-.-',    'L': '.-..',
+    'M': '--',     'N': '-.',     'O': '---',    'P': '.--.',
+    'Q': '--.-',   'R': '.-.',    'S': '...',    'T': '-',
+    'U': '..-',    'V': '...-',   'W': '.--',    'X': '-..-',
+    'Y': '-.--',   'Z': '--..',   '1': '.----',  '2': '..---',
+    '3': '...--',  '4': '....-',  '5': '.....',  '6': '-....',
+    '7': '--...',  '8': '---..',  '9': '----.',  '0': '-----',
+    ',': '--..--', '?': '..--..', ':': '---...', '&': '.-...',
+    ' ': ' '
+}
+
+def morse_code(english:str):
+    return ''.join([MORSE_CODE.get(x.upper(), '') for x in english])
+
+print(morse_code('Hello! My name is Heather!'))
